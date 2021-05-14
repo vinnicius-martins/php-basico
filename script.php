084c7b7bc96c6db8ff23311ce566e5b9297fd360
vinnicius-martins <vinnicius.o.martins@gmail.com>
2021-05-14 10:32:44 -0300

    Adicionando validação de campos do formulário

--- script.php
+++ script.php
@@ -11,6 +11,24 @@ $idade = $_POST['idade'];
 //var_dump($nome);
 //var_dump($idade);
 
+if(empty($nome)){
+    echo 'O nome não pode ser vazio';
+    return;
+}
+if(strlen($nome)<3){
+    echo 'O nome deve conter mais de 3 caracteres';
+    return;
+}
+if(strlen($nome)>40){
+    echo 'O nome é muito extenso';
+    return;
+}
+if(!is_numeric($idade)){
+    echo 'Informe um número para idade';
+    return;
+}
+
+
 if ($idade >= 6 && $idade <= 12){
     echo "O nadador ".$nome." compete na categoria infantil";
 }else if($idade > 12 && $idade <= 18){
